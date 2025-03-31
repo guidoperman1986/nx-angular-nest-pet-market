9f1c975f4106f6cdff3ce31ba4383048c89399c8
Guido Perman <guidoyaelperman@gmail.com>
2025-03-31 10:35:50 -0300

    Add product management features with NgRx signals and Apollo integration

--- apps/pet-market-web/src/app/products/products.component.ts
+++ apps/pet-market-web/src/app/products/products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { afterNextRender, Component, inject } from '@angular/core';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { ProductStore } from '../stores/products.store';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule],
+  imports: [CommonModule, JsonPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  productStore = inject(ProductStore);
+
+  constructor() {
+    afterNextRender(() => {
+      this.productStore.loadProducts();
+      
+      console.log(this.productStore.products())
+    });
+  }
+
+}
